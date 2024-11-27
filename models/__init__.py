0f4e0a938381d6cf0a612715c1400f47bbadd8c9
123 <e2753538973@outlook.com>
2024-11-27 22:21:30 +0800

    Add VMUNet model and update load_model function

--- models/__init__.py
+++ models/__init__.py
@@ -1,9 +1,11 @@
 from .unet import UNet
 from .model import self_net
+from .vmunet.vmunet import VMUNet
 
 __all__ = [
     'UNet',
-    'self_net'
+    'self_net',
+    'VMUNet',
 ]
 
 
@@ -12,5 +14,7 @@ def load_model(model_name):
         return UNet(n_channels=3, n_classes=4, bilinear=False)
     elif model_name == 'self_net':
         return self_net(n_channels=3, n_classes=4)
+    elif model_name == 'VMUNet':
+        return VMUNet()
     else:
         raise ValueError(f"Unknown model name: {model_name}")
