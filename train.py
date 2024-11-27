5bffccc9856e386b724f5be86b5edc6199247f02
123 <e2753538973@outlook.com>
2024-11-27 22:02:31 +0800

    Adjust default batch size to 16 in train.py

--- train.py
+++ train.py
@@ -200,7 +200,7 @@ def train_model(
 def get_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('--epochs', '-e', metavar='E', type=int, default=5, help='Number of epochs')
-    parser.add_argument('--batch-size', '-b', dest='batch_size', metavar='B', type=int, default=32, help='Batch size')
+    parser.add_argument('--batch-size', '-b', dest='batch_size', metavar='B', type=int, default=16, help='Batch size')
     parser.add_argument('--learning-rate', '-l', metavar='LR', type=float, default=1e-4, help='Learning rate', dest='lr')
     parser.add_argument('--load', '-f', type=str, default=False, help='Load model from a .pth file')
     parser.add_argument('--scale', '-s', type=float, default=1, help='Downscaling factor of the images')
